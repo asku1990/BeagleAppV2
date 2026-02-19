563f768e950651a0d5d5e45832b5d447bdc5fcea
Aki Kuivas <91662678+asku1990@users.noreply.github.com>
2026-02-19 16:53:08 +0200

    fix(e2e): define chromium playwright project

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "../../tests/e2e",
   reporter: [["list"], ["html", { open: "never" }]],
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        browserName: "chromium",
+      },
+    },
+  ],
   use: {
     baseURL: "http://localhost:3000",
     trace: "retain-on-failure",
